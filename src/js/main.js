9c239bb9f64815d9b8a95e5279e651a2925a4995
Ilya Leykin <imleykin@gmail.com>
2019-04-09 11:26:56 +0500

    products-slider refresh fix

--- src/js/main.js
+++ src/js/main.js
@@ -68,6 +68,7 @@ $(document).ready(function() {
   });
 
   mainSlider.trigger("refresh.owl.carousel");
+
   // mainSlider.on("loaded.owl.lazy", function() {
   //   mainSlider.trigger("refresh.owl.carousel");
   //   console.log("slider refreshed");
@@ -134,6 +135,8 @@ $(document).ready(function() {
     onInitialized: fixOwl,
     onRefreshed: fixOwl
   });
+
+  $(".products-slider__trigger").trigger("refresh.owl.carousel");
 
   $(".brands-slider__trigger").owlCarousel({
     loop: false,
